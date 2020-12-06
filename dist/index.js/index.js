02a708487678ccc3b4505cae053d5af63572f403
mstanciu552 <mstanciu552@gmail.com>
2020-12-06 14:09:33 +0200

    (setup): simple bot example

--- dist/index.js/index.js
+++ dist/index.js/index.js
@@ -1,1 +1,14 @@
-console.log("hello from typescript");
+"use strict";
+exports.__esModule = true;
+var discord_js_1 = require("discord.js");
+var dotenv = require("dotenv");
+dotenv.config();
+var client = new discord_js_1.Client();
+client.on('message', function (message) {
+    var reply = new discord_js_1.MessageEmbed()
+        .setTitle("The bot works")
+        .setDescription("You wrote: " + message.content);
+    return message.channel.send(reply);
+    return;
+});
+client.login(process.env.DISCORD_TOKEN);
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,17 @@
-console.log("hello from typescript");
+import { Client, MessageEmbed } from 'discord.js';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
+const client = new Client();
+
+client.on('message', message => {
+	const reply = new MessageEmbed()
+		.setTitle("The bot works")
+		.setDescription(`You wrote: ${ message.content}`);
+
+	return message.channel.send(reply);
+	return;
+
+})
+
+client.login(process.env.DISCORD_TOKEN);
