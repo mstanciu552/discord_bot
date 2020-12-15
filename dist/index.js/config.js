6526025fb938574f6c171113d1441b32cff70b62
mstanciu552 <mstanciu552@gmail.com>
2020-12-15 13:14:51 +0200

    Added bot functionality

---
+++ dist/index.js/config.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+exports["default"] = {
+    COLORS: {
+        message: '#0099ff',
+        alerts: '#e84118',
+        system: '#4cd137',
+        github: '#6f7c7d',
+        users: '#ffc200',
+        help: '#4b2c5e'
+    }
+};
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,21 @@
-import { Client, Message, MessageEmbed } from 'discord.js';
-import * as dotenv from 'dotenv';
+import { Client, Message, MessageEmbed } from "discord.js";
+import * as dotenv from "dotenv";
+import config from "./config";
 dotenv.config();
+
+console.log(config.COLORS.message);
 
 const client = new Client();
 
-client.on('message', (message : Message) => {
-	if (message.content === "bot test") {
-		const reply = new MessageEmbed()
-			.setTitle("The bot works")
-			.setDescription(`You wrote: ${ message.content}`);
-	
-		return message.channel.send(reply);
-	}
-	return;
+client.on("message", (message: Message) => {
+  if (message.content === "bot test") {
+    const reply = new MessageEmbed()
+      .setTitle("The bot works")
+      .setDescription(`You wrote: ${message.content}`);
 
-})
+    return message.channel.send(reply);
+  }
+  return;
+});
 
 client.login(process.env.DISCORD_TOKEN);
